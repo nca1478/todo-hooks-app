06be60b0d5e27c9fd69a4ef9729105355e42e180
Nelson Cadenas <nca1478@yahoo.com>
2020-12-18 15:39:00 -0400

    104. Inicio de proyecto - Hooks

---
+++ src/HookApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HookApp = () => {
+  return (
+    <div className="container">
+      <h2>HooksApp</h2>
+      <hr />
+    </div>
+  )
+}
+
+export default HookApp

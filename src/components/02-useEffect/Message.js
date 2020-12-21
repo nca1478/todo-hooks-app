ccac3a0bd9afc157ca54e75f8acf4207c6678d31
Nelson Cadenas <nca1478@yahoo.com>
2020-12-21 18:35:52 -0400

    109. useEffect - Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
+  const { x, y } = coords
+
   useEffect(() => {
-    console.log('Componente Montado')
+    const mouseMove = (e) => {
+      const { x, y } = { x: e.x, y: e.y }
+      setCoords({ x, y })
+    }
+
+    window.addEventListener('mousemove', mouseMove)
+
+    // console.log('Componente Montado')
     return () => {
       console.log('Componente Desmontado')
+      window.removeEventListener('mousemove', mouseMove)
     }
   }, [])
 
   return (
     <>
       <h1>Parangaricutirimicuaro</h1>
+      <p>
+        x: {x}, y: {y}
+      </p>
     </>
   )
 }

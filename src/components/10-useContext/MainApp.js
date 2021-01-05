5f34e2c7555dd00879bb89112c208e632e463d71
Nelson Cadenas <nca1478@yahoo.com>
2021-01-04 20:22:39 -0400

    140. CreateContext y useContext

--- src/components/10-useContext/MainApp.js
+++ src/components/10-useContext/MainApp.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
-  return <AppRouter />
+  const user = {
+    id: 1234,
+    name: 'Luke Skywalker',
+    email: 'luke.skywalker@starwars.com'
+  }
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  )
 }
